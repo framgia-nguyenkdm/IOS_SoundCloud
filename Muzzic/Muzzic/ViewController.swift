5f60688aa49621f4cb47afbe85c83012df34bf4a
Khuc Dinh Minh Nguyen <khuc.dinh.minh.nguyen@framgia.com>
2018-04-09 17:08:19 +0700

    Init project, add SwiftLint

---
+++ Muzzic/Muzzic/ViewController.swift
@@ -0,0 +1,16 @@
+//  ViewController.swift
+//  Muzzic
+//  Created by khuc.d.m.nguyen on 4/9/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
